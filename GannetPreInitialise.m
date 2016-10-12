9a4dab2b7a555f0dc6f966e34416b520b9e2484f
schorschinho <goeltzs1@jhmi.edu>
2016-10-12 16:21:48 -0400

    Included support for CMRR sequence on VE11C. Thanks to Hongji Chen and Laura Rowland (UMB).

--- GannetPreInitialise.m
+++ GannetPreInitialise.m
@@ -7,7 +7,7 @@ function MRS_struct=GannetPreInitialise(MRS_struct)
 %Acquisition Parameters
     MRS_struct.p.sw=2000;  % sw taken from header for all formats except Philips .data
    % MRS_struct.p.sw=5000; %This should be parsed from headers where possible
-    MRS_struct.p.npoints=1024; %This is twice the acquired points for TWIX data;
+    MRS_struct.p.npoints=2080; %This is twice the acquired points for TWIX data;
     %This should be parsed from headers where possible
     MRS_struct.p.TR=2000;%This should be parsed from headers where possible
     MRS_struct.p.TE=68; %This should be parsed from headers where possible
@@ -15,12 +15,12 @@ function MRS_struct=GannetPreInitialise(MRS_struct)
     %In general, LarmorFreq is 127.8 on Philips,
     MRS_struct.p.Nwateravg = 8; %Needed for GE
     MRS_struct.p.target='GABA'; %Other options are 'GSH' and 'Glx' (now also implemented) 'GABAGlx' in dev
-    MRS_struct.p.ONOFForder='offfirst';
+    MRS_struct.p.ONOFForder='onfirst';
     %Options are MRS_struct.ONOFForder='onfirst' or 'offfirst';
     MRS_struct.p.Water_Positive=1; %For Philips MOIST ws, set to 0.
     %Siemens header information differs between versions
     %switch for different versions
-    MRS_struct.p.Siemens_type = 1; %1 = TIM TRIO WIP 2 = Near seq 3 =Skyra WIP; 4=Prisma (VD13C); 5=Prisma(Minnesota); 6 = Jamie's VE11B (Jena)
+    MRS_struct.p.Siemens_type = 7; %1 = TIM TRIO WIP 2 = Near seq 3 =Skyra WIP; 4=Prisma (VD13C); 5=Prisma(Minnesota); 6 = Jamie's VE11B (Jena); 7 = Minnesota (VE11C)
     
 %Analysis Parameters
     MRS_struct.p.LB = 3;
